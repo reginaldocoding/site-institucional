dc9d8787a12b4baa8350982231abcbc5e919fcbf
Reginaldo L Jr <reginaldodev09@gmail.com>
2022-05-27 17:48:46 -0300

    Front-end implementado(com algumas correções básicas nas rotas)

---
+++ Site Institucional/site-institucional/resources/views/site/category/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.site')
+
+@section('content')
+    <header class="common-header">
+        <div class="header-wrapper">
+            <h1 class="title-large">With apples drink teriyaki. </h1>
+        </div>
+        <div class="pattern__header"></div>
+    </header>
+    <!-- Products list -->
+    <div class="gray-background">
+        <section class="products__cultivation__category main-wrapper">
+            
+        </section>
+    </div>
+@endsection
